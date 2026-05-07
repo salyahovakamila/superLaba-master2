c3e0525e25c9d63f140aefb69a92673d720c64bf
salyahovakamila <salyahova.k@yandex.ru>
2026-05-07 16:40:00 +0300

    Update RequestSender.java

--- src/client/network/RequestSender.java
+++ src/client/network/RequestSender.java
@@ -1,68 +1,86 @@
 package client.network;
 
 import client.utils.Constants;
-import common.CommandRequest;
-import common.CommandResponse;
-import common.SerializationHelper;
-
-import java.io.IOException;
-import java.nio.ByteBuffer;
-import java.nio.channels.SocketChannel;
+import java.io.*;
+import java.util.Objects;
 
 public class RequestSender {
+    private final TCPClient tcpClient;
+    private final String host;
+    private final int port;
 
-    public CommandResponse sendAndReceive(CommandRequest request) throws IOException {
-        SocketChannel channel = null;
+
+    public RequestSender() throws IOException {
+        this(Constants.DEFAULT_HOST, Constants.DEFAULT_PORT);
+    }
+
+
+    public RequestSender(String host, int port) throws IOException {
+        this.host = host;
+        this.port = port;
+        this.tcpClient = new TCPClient(host, port, Constants.MAX_RETRIES);
+    }
+
+
+    public RequestSender(TCPClient tcpClient) {
+        this.tcpClient = Objects.requireNonNull(tcpClient);
+        this.host = Constants.DEFAULT_HOST;
+        this.port = Constants.DEFAULT_PORT;
+    }
+
+    @SuppressWarnings("unchecked")
+    public <T> T sendAndReceive(Serializable request, Class<T> responseType)
+            throws IOException, ClassNotFoundException {
+
+        byte[] requestBytes = serialize(request);
+
         try {
-            channel = TCPClient.connect();
-            // Отправка
-            byte[] requestBytes = SerializationHelper.serialize(request);
-            ByteBuffer writeBuffer = ByteBuffer.allocate(4 + requestBytes.length);
-            writeBuffer.putInt(requestBytes.length);
-            writeBuffer.put(requestBytes);
-            writeBuffer.flip();
-            while (writeBuffer.hasRemaining()) {
-                channel.write(writeBuffer);
+            tcpClient.send(requestBytes);
+            byte[] responseBytes = tcpClient.receive();
+            return (T) deserialize(responseBytes);
+        } catch (IOException e) {
+            // Обработка временной недоступности
+            if (tcpClient.reconnect(host, port)) {
+                tcpClient.send(requestBytes);
+                byte[] responseBytes = tcpClient.receive();
+                return (T) deserialize(responseBytes);
             }
+            throw new IOException("Сервер недоступен после попытки переподключения", e);
+        }
+    }
 
-            // Приём ответа
-            ByteBuffer lengthBuffer = ByteBuffer.allocate(4);
-            long deadline = System.currentTimeMillis() + Constants.TIMEOUT_MS;
-            while (lengthBuffer.hasRemaining()) {
-                if (System.currentTimeMillis() > deadline) {
-                    throw new IOException("Таймаут чтения длины ответа");
-                }
-                int read = channel.read(lengthBuffer);
-                if (read == -1) throw new IOException("Сервер закрыл соединение");
-                if (read == 0) Thread.yield();
-            }
-            lengthBuffer.flip();
-            int dataLength = lengthBuffer.getInt();
-            if (dataLength <= 0 || dataLength > Constants.BUFFER_SIZE) {
-                throw new IOException("Некорректный размер данных: " + dataLength);
-            }
-
-            ByteBuffer dataBuffer = ByteBuffer.allocate(dataLength);
-            deadline = System.currentTimeMillis() + Constants.TIMEOUT_MS;
-            while (dataBuffer.hasRemaining()) {
-                if (System.currentTimeMillis() > deadline) {
-                    throw new IOException("Таймаут чтения данных ответа");
-                }
-                int read = channel.read(dataBuffer);
-                if (read == -1) throw new IOException("Сервер закрыл соединение");
-                if (read == 0) Thread.yield();
-            }
-            dataBuffer.flip();
-            byte[] responseBytes = new byte[dataLength];
-            dataBuffer.get(responseBytes);
-
-            CommandResponse response = SerializationHelper.deserialize(responseBytes, CommandResponse.class);
-            if (response == null) throw new IOException("Не удалось десериализовать ответ");
-            return response;
-        } finally {
-            if (channel != null && channel.isOpen()) {
-                channel.close();
+    public void sendOnly(Serializable request) throws IOException {
+        try {
+            tcpClient.send(serialize(request));
+        } catch (IOException e) {
+            if (tcpClient.reconnect(host, port)) {
+                tcpClient.send(serialize(request));
+            } else {
+                throw e;
             }
         }
+    }
+
+    private byte[] serialize(Serializable obj) throws IOException {
+        try (ByteArrayOutputStream baos = new ByteArrayOutputStream();
+             ObjectOutputStream oos = new ObjectOutputStream(baos)) {
+            oos.writeObject(obj);
+            return baos.toByteArray();
+        }
+    }
+
+    private Object deserialize(byte[] data) throws IOException, ClassNotFoundException {
+        try (ByteArrayInputStream bais = new ByteArrayInputStream(data);
+             ObjectInputStream ois = new ObjectInputStream(bais)) {
+            return ois.readObject();
+        }
+    }
+
+    public boolean isConnected() {
+        return tcpClient.isConnected();
+    }
+
+    public void close() throws IOException {
+        tcpClient.close();
     }
 }

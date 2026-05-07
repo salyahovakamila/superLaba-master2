03b97c7cd29aacd2c81c328492197449727e53e5
salyahovakamila <salyahova.k@yandex.ru>
2026-05-07 16:40:22 +0300

    Update Constants.java

--- src/client/utils/Constants.java
+++ src/client/utils/Constants.java
@@ -5,6 +5,9 @@ public class Constants {
         public static final int PORT =2424;
         public static final int TIMEOUT_MS = 10000;
         public static final int BUFFER_SIZE=8192;
-
-
+        public static final int RETRY_DELAY_MS = 1000;
+        public static final int MAX_RETRIES = 3;
+        public static final int MAX_MESSAGE_SIZE = 1024 * 1024;
+        public static final String DEFAULT_HOST = "localhost";
+        public static final int DEFAULT_PORT = 12345;
 }
